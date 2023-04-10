f59400658d3ebd0a26505d6bbc8860f33830bd92
Giuseppe <120139042+peppemig@users.noreply.github.com>
2023-04-10 23:26:43 +0200

    added next-auth functionality

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import Button from "@/components/ui/Button";
+import {FC} from "react"
+
+interface pageProps {
+
+}
+
+const page: FC<pageProps> = ({}) => {
+    return ( 
+        <Button>hello</Button>
+     );
+}
+ 
+export default page;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Button from "@/components/ui/Button"
+
+export default async function Home() {
+  return (
+    <Button variant='ghost'>Hello</Button>
+  )
+}
